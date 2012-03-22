babbe46c7a3c236f0dd9afa38c592b210ca2879e
Jamie Miller <jmiller@biozymeinc.com>
2012-03-22 11:59:46 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0196c9cf6fb188c8a0a664f2c46a23b41e48b0264a48fd2c8a6eb1616a7976abdc59fd9334ff8076a779d641c9711e896dc76526820c3ee7613a5c2db5a53baf'
